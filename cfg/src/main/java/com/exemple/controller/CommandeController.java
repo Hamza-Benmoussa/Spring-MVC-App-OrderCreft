322b6ba4513877675010da6f1da33ee9d57cb11e
hamza <hamzabenmoussa166@gmail.com>
2024-01-01 20:30:50 +0100

    the modification in rapport but not complited

--- cfg/src/main/java/com/exemple/controller/CommandeController.java
+++ cfg/src/main/java/com/exemple/controller/CommandeController.java
@@ -5,14 +5,12 @@ import com.exemple.entity.Commande;
 import com.exemple.entity.OrderProduit;
 import com.exemple.entity.Produit;
 import com.exemple.service.InterfaceClientService;
-import com.exemple.service.InterfaceCommandeProduitService;
 import com.exemple.service.InterfaceCommandeService;
 import com.exemple.service.InterfaceProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -117,6 +115,6 @@ public class CommandeController {
         }
 
         model.addAttribute("commandes", commandes);
-        return "commande/rapport";
+        return "rapport"; // Return the rapport.html template
     }
 }
